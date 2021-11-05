5c53837ce0eb0d7faeb6b22a7b19e654da6f9855
Tristan Johns <tfj@protonmail.com>
2021-11-05 10:16:10 -0600

    create

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -6,14 +6,27 @@ export class PostsController extends BaseController {
   constructor() {
     super('api/posts')
     this.router
+      .get('', this.getAll)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.create)
   }
 
+  async getAll(req, res, next) {
+    try {
+      const query = req.query
+      const posts = await postsService.getAll(query)
+      return res.send(posts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
   async create(req, res, next) {
     try {
-    // cannot modify creatorId with body
+      // cannot modify creatorId with body
       req.body.creatorId = req.userInfo.id
+      // cannot initially send fake likes
+      req.body.likes = 1
       const post = await postsService.create(req.body)
       return res.send(post)
     } catch (error) {
--- server/services/PostsService.js
+++ server/services/PostsService.js
@@ -2,6 +2,12 @@ import { dbContext } from '../db/DbContext'
 import { BadRequest, Forbidden } from '../utils/Errors'
 
 class PostsService {
+  async getAll(query = {}) {
+    const posts = await dbContext.Posts.find(query)
+      .populate('creator', 'name picture')
+    return posts
+  }
+
   async create(newPost) {
     const post = await dbContext.Posts.create(newPost)
     return post
